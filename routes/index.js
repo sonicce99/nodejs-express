a78e00efc3a89b429b523d91e1c1661d9342e5e9
dongsu <sonicce99@naver.com>
2022-11-03 11:06:51 +0900

    docs  : 기본 라우팅

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get(
+  "/example/b",
+  function (req, res, next) {
+    res.send("the response will be sent by the next function ...");
+    next();
+  },
+  function (req, res) {
+    res.send("Hello from B!");
+  }
+);
+
+var cb0 = function (req, res, next) {
+  console.log("CB0");
+  next();
+};
+
+var cb1 = function (req, res, next) {
+  console.log("CB1");
+  next();
+};
+
+var cb2 = function (req, res) {
+  res.send("Hello from C!");
+};
+
+router.get("/example/c", [cb0, cb1, cb2]);
 
 module.exports = router;
